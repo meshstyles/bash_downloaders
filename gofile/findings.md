bd3ee8d4e3375a144b5e03a643cc20838a46ae9c
Max M <meshstyles@users.noreply.github.com>
2022-02-19 18:05:45 +0100

    adding gofile support

---
+++ gofile/findings.md
@@ -0,0 +1,42 @@
+# gofile
+
+## create account api
+
+`https://api.gofile.io/createAccount`
+
+```bash
+curl "https://api.gofile.io/createAccount" | jq '.data.token'
+```
+
+## verify token validity
+
+`https://api.gofile.io/getAccountDetails?token=$token`  
+if `.status` of the response is 'ok' then the token was valid.  
+ideally the token gets saved to reduce the need to obtain new tokens which might look like unusual user behaviour.
+
+## content api
+
+we need to make an request with to the api with the following api params  
+`https://api.gofile.io/getContent?contentId=$FOLDERID&token=$TOKEN&websiteToken=websiteToken`
+
+contentId: $FOLDERID {string}  
+token: $TOKEN {string}  
+websiteToken: "websiteToken"  
+
+### Whats important for file downloads
+
+the link and name are required, if we obtain the name from the api object we can save ourselves parsing of the url.
+
+```
+.data.contents."UUID".link
+.data.contents."UUID".name
+```
+
+### UUIDs
+
+the UUIDs are required to access the links and names for files.  
+the UUIDs are in the .data.childs array.
+
+```
+.data.childs[]
+```
---
+++ gofile/script.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+link=$1
+useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.27"
+
+#link "gofile.io/d/"
+#cutoff
+
+gofile_verifytoken(){
+    cached_token=$(cat "$tokenfile" | jq -r '.token')
+    token_verification_request_status=$(curl "https://api.gofile.io/getAccountDetails?token=$token" | jq '.status')
+    if [[ "$token_verification_request_status" == 'ok' ]]; then
+        token="$cached_token"
+        token_set="true"
+    fi
+}
+
+gofile_you_want_to_continue(){
+    echo "do you want to continue: \"yes\" "
+    read continue_gofile
+
+    if [[ "$continue_gofile" != "yes" ]]; then
+        exit 0
+    fi
+}
+
+# if mkdir fails stop
+[ -d ~/.local/cache/ ] || mkdir ~/.local/cache/ || exit 1 
+
+tokenfile="/home/$(whoami)/.local/cache/gofileio.token"
+[ -f "$tokenfile" ] && gofile_verifytoken
+[ -f "$tokenfile" ] || touch "$tokenfile"
+
+if [[ "$token_set" != "true" ]]; then
+    token=$(curl -H "User-Agent: ${useragent}" "https://api.gofile.io/createAccount" | jq -r '.data.token')
+    echo "{\"token\":\"$token\"}" > "$tokenfile"
+fi 
+
+contentId=$(echo "${link##*\/d\/}" | cut -d '?' -f 1 | cut -d '/' -f 1)
+echo "tokens : $token"
+echo "contentId : $contentId"
+
+mkdir "gofolder($contentId)" || gofile_you_want_to_continue
+cd "gofolder($contentId)"
+
+url="https://api.gofile.io/getContent?contentId=${contentId}&token=${token}&websiteToken=websiteToken"
+api_file_response=$(curl -H "User-Agent: ${useragent}" "$url")
+
+for i in  $(echo "$api_file_response" | jq -r '.data.childs | keys | .[]'); do
+    child=$(echo "$api_file_response" | jq -r ".data.childs[$i]")
+    search_child=".\"$child\""
+    file_object=$(echo "$api_file_response" | jq '.data.contents' | jq "$search_child")
+
+    file_name=$(echo "$file_object" | jq -r '.name')
+    file_directLink=$(echo "$file_object" | jq -r '.link')
+
+    echo "========================="
+    echo "$file_name"
+    echo "$file_directLink"
+    echo "========================="
+
+    curl -H "User-Agent: ${useragent}" \
+    -H 'referer: https://gofile.io/' \
+    --cookie "accountToken=${token}" \
+    "$file_directLink" -o "$file_name"
+
+done
+exit 0
